d107b42f953bacc4623f30a3a2c045eafae66f5b
HENDRA-PC <HENDRA-PC@25.22.165.92>
2015-10-17 11:26:47 +0700

    (new) user interface - master block category | By Hendra A. Laksmana

--- application/views/admin/form_block_cat_add.php
+++ application/views/admin/form_block_cat_add.php
@@ -31,12 +31,7 @@
                                 <input type="text" class="form-control" id="block_desc" placeholder="Type Block Description Here..">
                             </div>
                         </div>
-                        <div class="col-md-12">
-                            <div class="text-center">
-                                <a onclick="goBack()" class="btn ink-reaction btn-floating-action btn-sm btn-primary"><i class="fa fa-reply" title="Back to Previous Page"></i></a>
-                                <button class="btn ink-reaction btn-floating-action btn-sm btn-success" type="submit"><i class="fa fa-save" title="Save Data!"></i></button>
-                            </div>
-                        </div>
+                        <?php $this->load->view('admin/action_button_partial/btn_for_submit'); ?>
                     </div>
                     </form>
                 </div>
